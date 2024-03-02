f3507a656fbac473ba3a96a8152d51bbf6ca57d5
M0Hani <105191918+M0Hani@users.noreply.github.com>
2024-03-02 21:49:01 +0200

    Changes to be committed:     new file:   gp_project/lib/recorder.dart

---
+++ gp_project/lib/recorder.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:neon_widgets/neon_widgets.dart';
+
+class RecScreen extends StatefulWidget
+{
+  @override
+  _RecScreenState createState() => _RecScreenState();
+}
+
+class _RecScreenState extends State<RecScreen>
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
